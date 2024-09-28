63dbc30f2759830fc35e59083c77983035c6a666
mahak <minime031296@gmail.com>
2024-09-28 13:11:05 +0530

    added code

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended: true}))
 
 //cors middleware
 const corsOption = {
-    origin: ['https://vistaar-webx.vercel.app/'],
+    origin: 'https://vistaar-webx.vercel.app',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }
