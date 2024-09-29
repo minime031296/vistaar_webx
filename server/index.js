95e7f5e3cbfa5530c0f4b606d31bc0914b406276
mahak <minime031296@gmail.com>
2024-09-29 18:16:17 +0530

    server commit

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended: true}))
 //cors middleware
 
 const corsOption = {
-    origin: 'https://vistaar-webx.vercel.app',
+    origin: 'http://localhost:3001',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }
